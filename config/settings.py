6edc7a042ceb55b3c9defc35d2531d4e374313ec
imad <imadeddine.djekoune@eadn.dz>
2026-01-07 14:28:09 +0100

    change db from sqlite to pgadmin

--- config/settings.py
+++ config/settings.py
@@ -73,11 +73,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "neondb",
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_OrWbUuMoyL79',
+        'HOST': 'ep-falling-feather-ah3hlf4q-pooler.c-3.us-east-1.aws.neon.tech',
+        'PORT': '5432',
     }
+
 }
 
 
