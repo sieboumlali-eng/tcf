bb80d4c49b99e1c1fda410a3bf0c31dacaa88cea
imad <imadeddine.djekoune@eadn.dz>
2026-01-07 14:56:41 +0100

    make it lazy

--- revision/urls.py
+++ revision/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.exam_list, name='exam_list'),
     path('exam/<int:exam_id>/', views.exam_detail, name='exam_detail'),
+    path('api/exam/<int:exam_id>/question/<int:question_num>/', views.question_api, name='question_api'),
 ]
--- revision/views.py
+++ revision/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-
-from .models import ExamOrale
-
 from django.shortcuts import render, get_object_or_404
-from .models import ExamOrale
+from django.http import JsonResponse
+from .models import ExamOrale, QuestionOrale
 
 def exam_list(request):
     exams = ExamOrale.objects.all()
@@ -11,10 +8,34 @@ def exam_list(request):
 
 def exam_detail(request, exam_id):
     exam = get_object_or_404(ExamOrale, pk=exam_id)
-    questions = exam.questions.all().order_by('num_qst')
+    # Only get question numbers, not the heavy data
+    questions = exam.questions.all().order_by('num_qst').values('num_qst', 'score')
     
     context = {
         'exam': exam,
-        'questions': questions
+        'questions': list(questions),
+        'total_questions': exam.questions.count()
     }
     return render(request, 'revision/index.html', context)
+
+def question_api(request, exam_id, question_num):
+    """API endpoint to fetch a single question's data"""
+    question = get_object_or_404(
+        QuestionOrale, 
+        exam_id=exam_id, 
+        num_qst=question_num
+    )
+    
+    data = {
+        'num_qst': question.num_qst,
+        'text_qst': question.text_qst,
+        'image_qst': question.image_qst or '',
+        'audio_data_url': question.audio_data_url or '',
+        'choice_a': question.choice_a,
+        'choice_b': question.choice_b,
+        'choice_c': question.choice_c,
+        'choice_d': question.choice_d,
+        'score': question.score,
+        'solution_qst': question.solution_qst,
+    }
+    return JsonResponse(data)
