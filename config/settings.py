0b73e20d7771f47096d2467d8cd04fd0fa862f51
imad <imadeddine.djekoune@eadn.dz>
2026-01-07 14:05:42 +0100

    test init

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-)68njkc93ivlxto()q1sgooj*2(q^*gcuyx#j=xcx%s8z@@$=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*",".vercel.app",".now.sh","127.0.0.1","localhost"]
 
 # Application definition
 
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'project/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
